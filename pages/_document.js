4c7ad28e690c24c64d3281fabc0e4ed7ea6b4346
tkay6677 <barrykage@gmail.com>
2025-06-14 16:42:28 -0700

    package edit 4

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* TinyMCE CDN Script */}
-          <script src={`https://cdn.tiny.cloud/1/${process.env.NEXT_PUBLIC_TINYMCE_API_KEY}/tinymce/6/tinymce.min.js`} referrerPolicy="origin" />
+          
         </Head>
         <body>
           <Main />
